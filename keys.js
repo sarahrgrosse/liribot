0714ff79862440e043c7b38c423389dc3be843c5
Sarah Rhianna Grosse <sarahrgrosse@sarahs-air.attlocal.net>
2019-11-03 14:50:58 -0500

    adding code to files

--- keys.js
+++ keys.js
@@ -1,3 +1,9 @@
+//This is calling the keys varibale from the keys.js file
+var keys = require("./keys.js");
+
+//This is allowing access to my keys information
+var spotify = new spotify(keys.spotify);
+
 console.log('this is loaded');
 
 exports.spotify = {
